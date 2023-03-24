e52a41c868dc981aa11dea156ae88d66def6333b
Oskar Westmeijer <oskar.westmeijer@gmail.com>
2023-03-24 18:53:21 +0100

    feat: automate deployment

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,22 +1,4 @@
-terraform {
-  required_providers {
-    google = {
-      source  = "hashicorp/google"
-      version = "4.51.0"
-    }
-  }
-}
-
-provider "google" {
-  credentials = file("westmeijer-oskar-project-creds.json")
-
-  project = var.project_id
-  region  = var.project_region
-  zone    = var.project_zone
-}
-
-
-# overview in Service Usage API
+# enables service apis, overview in Service Usage API
 resource "google_project_service" "gcp_services" {
   for_each                   = toset(var.gcp_service_list)
   project                    = var.project_id
